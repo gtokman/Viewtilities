32e4b79135a5693271d977d132d1082361ba2ea4
Gary <Tokman>
2019-01-18 16:05:41 -0500

    Add example app

--- Example/Viewtilities/ViewController.swift
+++ Example/Viewtilities/ViewController.swift
@@ -7,18 +7,31 @@
 //
 
 import UIKit
+import Viewtilities
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        let myView = UIView()
+        // Set corner radius
+        myView.round(to: 10)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        // Set shadow
+        myView.shadow(offset: .init(width: 0, height: 2), radius: 5, opacity: 0.5)
 
+        // Set border
+//        myView.border(width: 1)
+
+        myView.backgroundColor = .lightGray
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(myView)
+
+        NSLayoutConstraint.activate([
+            myView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            myView.heightAnchor.constraint(equalToConstant: 100),
+            myView.widthAnchor.constraint(equalToConstant: 100),
+            ])
+    }
 }
-
