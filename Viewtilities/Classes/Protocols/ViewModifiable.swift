28fb619c39451c751a18cee0011b3687d6328391
Gary <Tokman>
2019-01-19 22:46:36 -0500

    Add variadic add func

--- Viewtilities/Classes/Protocols/ViewModifiable.swift
+++ Viewtilities/Classes/Protocols/ViewModifiable.swift
@@ -5,10 +5,8 @@
 //  Created by Gary Tokman on 1/19/19.
 //
 
-import Foundation
-
 public protocol ViewModifiable {
-    func add(_ modifier: ViewModifier)
+    func add(_ modifier: ViewModifier...)
 }
 
 public extension ViewModifiable where Self: UIView {
@@ -16,7 +14,11 @@ public extension ViewModifiable where Self: UIView {
     /// Adds a `ViewModifier` to a view
     ///
     /// - Parameter modifier: A view modifier
-    func add(_ modifier: ViewModifier) {
+    func add(_ modifiers: ViewModifier...) {
+        modifiers.forEach(add(_:))
+    }
+
+    internal func add(_ modifier: ViewModifier) {
         switch modifier {
         case let .shadow(type):
             shadow(ofType: type)
@@ -24,6 +26,12 @@ public extension ViewModifiable where Self: UIView {
             round(ofType: type)
         case let .border(type):
             border(ofType: type)
+        case let .background(color, state):
+            guard let self = self as? UIButton else {
+                assertionFailure("Setting a background with a state is only available for UIButtons")
+                return
+            }
+            self.background(color: color, for: state)
         }
     }
 }
